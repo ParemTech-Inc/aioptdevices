60cbf9b3046c7f752676b1e63d99d2637a74ee18
Matthew Gibson <matthew.gibzon@gmail.com>
2025-11-03 16:30:00 +0000

    Removed try-catch from interface.py
    The calling program should be able to handle any exceptions that are
    thrown, and if not, allow the original through.

--- aioptdevices/interface.py
+++ aioptdevices/interface.py
@@ -48,48 +48,49 @@ class Interface:
             self.config.device_id,
         )
 
-        try:
-            async with self.config.session.request(
-                "get",
-                url,
-            ) as results:
-                LOGGER.debug(
-                    "%s Received from %s, %s", results.status, self.config.url, results
-                )
+        async with self.config.session.request(
+            "get",
+            url,
+        ) as results:
+            LOGGER.debug(
+                "%s Received from %s, %s", results.status, self.config.url, results
+            )
 
+            # Check return code
+            if results.status == HTTPStatus.UNAUTHORIZED:  # 401
+                raise PTDevicesUnauthorizedError(
+                    f"Request to {url} failed, the token provided is not valid"
+                )
                 # Check return code
                 if results.status == HTTPStatus.UNAUTHORIZED:  # 401
                     raise PTDevicesUnauthorizedError(
                         f"Request to {url} failed, the token provided is not valid"
                     )
 
-                if results.status == HTTPStatus.FORBIDDEN:  # 403
-                    raise PTDevicesForbiddenError(
-                        f"Request to {url} failed, token invalid for device {self.config.device_id}"
-                    )
-
-                if results.status != HTTPStatus.OK:  # anything but 200
-                    raise PTDevicesRequestError(
-                        f"Request to {url} failed, got unexpected response from server ({results.status})"
-                    )
-
-                # Check content type
-                if (
-                    results.content_type != "application/json"
-                    or results.content_length == 0
-                ):
-                    raise PTDevicesRequestError(
-                        f"Failed to get device data, returned content is invalid. Type: {results.content_type}, content Length: {results.content_length}, content: {results.content}"
-                    )
-
-                raw_json = await results.read()
-
-                body = orjson.loads(raw_json)
-
-                return PTDevicesResponse(
-                    code=results.status,
-                    body=body["data"],
+            elif results.status == HTTPStatus.FORBIDDEN:  # 403
+                raise PTDevicesForbiddenError(
+                    f"Request to {url} failed, token invalid for device {self.config.device_id}"
                 )
 
-        except client_exceptions.ClientError as error:
-            raise PTDevicesRequestError(f"Request to {url} failed: {error}") from error
+            elif results.status != HTTPStatus.OK:  # anything but 200
+                raise PTDevicesRequestError(
+                    f"Request to {url} failed, got unexpected response from server ({results.status})"
+                )
+
+            # Check content type
+            elif (
+                results.content_type != "application/json"
+                or results.content_length == 0
+            ):
+                raise PTDevicesRequestError(
+                    f"Failed to get device data, returned content is invalid. Type: {results.content_type}, content Length: {results.content_length}, content: {results.content}"
+                )
+
+            raw_json = await results.read()
+
+            body = orjson.loads(raw_json)
+
+            return PTDevicesResponse(
+                code=results.status,
+                body=body["data"],
+            )
